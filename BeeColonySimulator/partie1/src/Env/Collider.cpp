756758e17b0e28a781ac3383cdcd5e92c9d7a755
naulitus <94896339+leobruneau@users.noreply.github.com>
2022-03-22 10:52:56 +0100

    Add files via upload

---
+++ BeeColonySimulator/partie1/src/Env/Collider.cpp
@@ -0,0 +1,16 @@
+/*
+ * POOSV EPFL 2022
+ * @authors:
+ * Marco Antognini
+ */
+
+
+#include "Collider.hpp"
+#include <Utility/Constants.hpp>
+#include <Utility/Vec2d.hpp>
+
+Collider::Collider(Vec2d coordinates, double rad) {
+
+}
+
+/* TO BE COMPLETED */
---
+++ BeeColonySimulator/partie1/src/Env/Collider.hpp
@@ -0,0 +1,21 @@
+/*
+ * POOSV EPFL 2022
+ * @authors: 
+ */
+
+#pragma once
+
+#include <Utility/Vec2d.hpp>
+#include <iostream>
+
+/* TO BE COMPLETED */
+
+class Collider {
+private:
+    Vec2d position;
+    double radius;
+public:
+    Collider(Vec2d coordinates, double rad);
+
+};
+
