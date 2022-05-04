0a43a0198c4013e3967a2f03d541a104a1fee58f
naulitus <leo.bruneau@epfl.ch>
2022-05-04 16:32:42 +0200

    Done set up for part 4 (created Hive class and Bee class)

---
+++ BeeColonySimulator/partie4/src/Env/Bee.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Léo Bruneau on 04/05/22.
+//
+
+#ifndef BEE_COLONY_SIMULATOR_SSV_2022_STEP4_BEE_HPP
+#define BEE_COLONY_SIMULATOR_SSV_2022_STEP4_BEE_HPP
+
+
+class Bee {
+public:
+private:
+};
+
+
+#endif //BEE_COLONY_SIMULATOR_SSV_2022_STEP4_BEE_HPP
